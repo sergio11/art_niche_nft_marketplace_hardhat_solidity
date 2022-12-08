5682027b559144e0d2929a2202d559617f5b2f8c
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-12-08 19:59:03 +0100

    Implement deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,23 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const ContractFactory = await ethers.getContractFactory("ArtCollectible");
 
-  const instance = await ContractFactory.deploy();
-  await instance.deployed();
+  const utilsContractFactory = await ethers.getContractFactory("Utils")
+  const utilsContractInstance = await utilsContractFactory.deploy()
+  const artCollectibleContractFactory = await ethers.getContractFactory("ArtCollectibleContract")
+  const artCollectibleContractInstance = await artCollectibleContractFactory.deploy()
+  const artMarketplaceContractFactory = await ethers.getContractFactory("ArtMarketplaceContract", {
+    libraries: {
+      Utils: utilsContractInstance.address,
+    },
+  })
+  const artMarketplace = await artMarketplaceContractFactory.deploy()
+  await artMarketplace.setArtCollectibleAddress(artCollectibleContractInstance.address)
+  await artCollectibleContractInstance.setMarketPlaceAddress(artMarketplace.address)
+  await artMarketplace.deployed()
 
-  console.log(`Contract deployed to ${instance.address}`);
+  console.log(`ArtMarketplace contract deployed to ${artMarketplace.address}`);
+  console.log(`ArtCollectible contract deployed to ${artCollectibleContractInstance.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
